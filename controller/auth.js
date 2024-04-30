568fef68f4a71e53b490f2425a3513ff5a54c0ca
Sean <integin1212@naver.com>
2024-04-30 14:51:08 +0900

    회원가입, 로그인 수정

--- controller/auth.js
+++ controller/auth.js
@@ -15,7 +15,7 @@ async function makeToken(id){
 export async function signup(req, res, next){
     const {username, password, name, email} = req.body;
     const hashed = bcrypt.hashSync(password, 10);
-    const users = await authRepository.createUser(username, password, name, email);
+    const users = await authRepository.createUser(username, hashed, name, email);
     if(users){
         res.status(201).json(users);
     }
@@ -28,7 +28,7 @@ export async function login(req, res, next){
         if(bcrypt.compareSync(password, user.password)){
             res.status(201).header('Token', makeToken(username)).json(`${username} 로그인 완료`)
         }else{
-            res.status(404).josn({message: `${username}님 아이디 또는 비밀번호 확인하세요.`})
+            res.status(404).json({message: `${username}님 아이디 또는 비밀번호 확인하세요.`})
         }
     }else{
         res.status(404).json({message: `${username}님 아이디 또는 비밀번호 확인하세요`})
